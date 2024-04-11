85c54f90968e969fd499a0a990c238483590ea62
ferderplays <86313430+ferderplays@users.noreply.github.com>
2024-04-11 18:25:25 +0200

    nice

--- src/obj/Debug/net6.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net6.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cdc63c79ba07096a8f5a95cfee256ca2af0f245")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11f3b4406f76df11d9ad50ab2de10d703c2f2a26")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
